5d237d7cfc798d2eee5d41bb1f460aa14d34438a
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-12-04 01:07:04 +0530

    add basic validation for sign up form II

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/
 import {Title} from "@angular/platform-browser";
 import {ToastrService} from "ngx-toastr";
 import {User} from "../dto/user";
+import {NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
@@ -34,4 +35,29 @@ export class SignUpComponent implements OnInit, AfterViewInit {
     setTimeout(() => this.txtNameElmRef.nativeElement.focus(), 0)
   }
 
+  getErrorMsg(model: NgModel): string {
+    switch (model.name) {
+      case 'name':
+        if (model.hasError('required')) {
+          return "Name can't be empty";
+        } else if (model.hasError('pattern')) {
+          return 'Name can contain only letters & spaces';
+        }
+        break;
+      case 'user-id':
+        if (model.hasError('required')) {
+          return "User ID  can't be empty";
+        } else if (model.hasError('minLength')) {
+          return 'User ID should be at least 5 characters long';
+        } else if (model.hasError('pattern')) {
+          return "User ID can't be contains the colan (:)";
+        }
+        break;
+      case 'confirm-password':
+        if (model.hasError('required')) {
+          return "Repeated password can't be empty";
+        }
+    }
+    return '';
+  }
 }
