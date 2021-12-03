769c8f4345f2892125870f243d7ba5a572cc4ada
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-12-04 00:40:20 +0530

    add basic validation for sign up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,26 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {ToastrService} from "ngx-toastr";
+import {User} from "../dto/user";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
+
+// type a = User & {confirmPassword: string};
+
 export class SignUpComponent implements OnInit, AfterViewInit {
 
   @ViewChild("txtName")
   private txtNameElmRef!: ElementRef<HTMLInputElement>;
+  registrationDetail: User & { confirmPassword: string } = {
+    userId: '',
+    name: '',
+    password: '',
+    confirmPassword: ''
+  }
 
   constructor(private titleService: Title, private toastrService: ToastrService) {
   }
